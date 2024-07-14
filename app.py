c44a6bfd9569da572017017ae4e5c794d4d8d497
Carlos André <casl2@aluno.ifal.edu.br>
2024-07-14 14:41:05 -0300

    update

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from checker import *
 import streamlit as st
 
-col1, col2 = st.columns(2)
-uploaded_file = col1.file_uploader("Choose a file")
-if uploaded_file is not None:
-	col1.markdown(check(uploaded_file), unsafe_allow_html=True)
-	col2.image(uploaded_file, width = 350)
+program = st.sidebar.selectbox('Select program',['APP', 'Info'])
+
+if program == 'APP':
+	
+	col1, col2 = st.columns(2)
+	col1.title("Upload")
+	uploaded_file = col1.file_uploader("Choose a file")
+	
+	
+	if not uploaded_file:
+		st.error("Insert an image")
+		st.stop()
+		
+	if uploaded_file:
+		col1.title("Info")
+		col1.markdown(check(uploaded_file), unsafe_allow_html=True)
+
+		col2.title("Image")
+		col2.image(uploaded_file, width = 350)
